f1729edbe4fdf1f8ac86d223ea1f06f53abecc99
Bhoj Sao <bhojsao@ymail.com>
2019-09-14 18:38:36 +0530

    Added code for registering with eureka

--- src/main/java/com/bhoj/springboot/SpringBootHibenateEnversApp.java
+++ src/main/java/com/bhoj/springboot/SpringBootHibenateEnversApp.java
@@ -3,6 +3,7 @@ package com.bhoj.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication(scanBasePackages = { "com.bhoj.springboot" })
 @EntityScan( basePackages = {"com.bhoj.springboot.domain"} )
 @EnableJpaAuditing
+@EnableEurekaClient
 public class SpringBootHibenateEnversApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHibenateEnversApp.class, args);
